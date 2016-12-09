8b9fb17ef01ee68408670356667f4ec3d5cf48c8
natachit <natachit@hotmail.com>
2016-12-09 19:58:51 +0700

    "minus heart when the answer is wrong"

--- you-bet-me.py
+++ you-bet-me.py
@@ -31,12 +31,14 @@ class YouBetMeWindow(arcade.Window):
         arcade.draw_text(str(self.choice.list[self.world.question-1][1]),
                          545, 325, arcade.color.BLACK, 30, width=223, align="center",
                          anchor_x="center", anchor_y="center")
-        arcade.draw_text(self.world.input, 400, 180,
-                         arcade.color.BLACK, 20)
+        if (not self.world.is_ans):
+            arcade.draw_text(str(self.world.bet), 400, 180, arcade.color.RED, 20)
+        if (self.world.is_ans):
+            arcade.draw_text(str(self.world.bet), 400, 180, arcade.color.BLACK, 20)
 
     def on_key_press(self, key, key_modifiers):
-        choice = self.choice.random_answer()
-        self.world.on_key_press(key, key_modifiers, choice)
+        right_choice = self.choice.random_answer()
+        self.world.on_key_press(key, key_modifiers, right_choice)
 
 class World():
     def __init__(self):
@@ -44,23 +46,48 @@ class World():
         self.question = 1
         self.ans = 0
         self.input = ''
-        self.num_key = {48:'0', 49:'1', 50:'2', 51:'3', 52:'4',
-                        53:'5', 54:'6', 54:'7', 56:'8', 57:'9'}
+        #self.num_key = {48:'0', 49:'1', 50:'2', 51:'3', 52:'4', 53:'5', 54:'6', 54:'7', 56:'8', 57:'9'}
+        self.num_key = {48:0, 49:1, 50:2, 51:3, 52:4, 53:5, 54:6, 55:7, 56:8, 57:9}
         self.bet = 0
+        self.is_ans = False
+        self.tmp = 0
 
-    def on_key_press(self, key, key_modifiers, choice):
-        if key == arcade.key.LEFT:
+    def on_key_press(self, key, key_modifiers, right_choice):
+        if key == arcade.key.LEFT and self.is_ans:
             self.ans = 1
-        if key == arcade.key.RIGHT:
+            self.check_ans(right_choice)
+        if key == arcade.key.RIGHT and self.is_ans:
             self.ans = 2
-        if choice == self.ans - 1:
-            self.question += 1
+            self.check_ans(right_choice)
         if key == arcade.key.BACKSPACE:
-            self.input = self.input[:-1]
+            self.convert_input(False, tmp)
         if 48 <= key <= 57:
-            tmp = str(self.num_key[key])
-            self.input += tmp
-            self.bet = int(self.input)
+            tmp = self.num_key[key]
+            self.convert_input(True, tmp)
+        if int(self.bet) >= 2000:
+            self.is_ans = True
+
+    def reset(self):
+        self.ans = 0
+        self.input = ''
+        self.is_ans = False
+        self.bet = 0
+        self.tmp = 0
+
+    def check_ans(self, right_choice):
+        if (self.ans == right_choice):
+            self.heart += self.bet
+            self.question += 1
+        if (self.ans != right_choice):
+            self.heart -= self.bet
+        self.reset()
+
+    def convert_input(self, is_value, value):
+        if not is_value:
+            self.bet = (self.bet-value)/10
+        else:
+            self.bet = self.bet*10+value
+
 
 
  
