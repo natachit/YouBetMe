03320fa368d078148573e3ff93527d7e405469a7
natachit <natachit@hotmail.com>
2016-12-09 17:07:01 +0700

    add box

--- you-bet-me.py
+++ you-bet-me.py
@@ -7,11 +7,17 @@ class YouBetMeWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
  
-        arcade.set_background_color(arcade.color.BLACK)
+        arcade.set_background_color(arcade.color.WHITE)
+        self.box = arcade.Sprite('images/box.png')
+        self.box.set_position(305, 325)
+        self.box2 = arcade.Sprite('images/box.png')
+        self.box2.set_position(545, 325)
  
  
     def on_draw(self):
         arcade.start_render()
+        self.box.draw()
+        self.box2.draw()
  
  
 if __name__ == '__main__':
